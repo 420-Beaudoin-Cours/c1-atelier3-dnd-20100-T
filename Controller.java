f4122ba184195bb7b919d9354175cbfe0d731060
Vincent12345 <vinct27@hotmail.ca>
2020-02-04 16:49:24 -0500

    Exercise 1

--- Controller.java
+++ Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.scene.control.TextField;
+import javafx.scene.input.*;
+
 public class Controller {
+    public TextField txfSource;
+
+    public void txfDragOnDetected(MouseEvent mouseEvent) {
+
+        TextField ref = (TextField) mouseEvent.getSource();
+        if (!ref.getText().isEmpty()) {
+            Dragboard db = txfSource.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(txfSource.getText());
+            db.setContent(content);
+        }
+    }
+
+    public void imgOnDragDrop(DragEvent dragEvent) {
+        Dragboard db = dragEvent.getDragboard();
+        txfSource.clear();
+
+    }
+
+    public void imgDragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
 }
